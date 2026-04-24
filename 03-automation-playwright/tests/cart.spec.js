3ef003de078880dcba985dd2dfb2cecb4aa5aa4d
Ezekeil Third Cruz <ezekielcruz0917@gmail.com>
2026-04-24 16:34:18 +0800

    Updated test cases and fixes

--- 03-automation-playwright/tests/cart.spec.js
+++ 03-automation-playwright/tests/cart.spec.js
@@ -12,3 +12,37 @@ test('Add item to cart', async ({ page }) => {
   const cartBadge = await page.locator('.shopping_cart_badge');
   await expect(cartBadge).toHaveText('1');
 });
+
+test('Add more items to cart', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+
+  
+  await page.click('[data-test ="add-to-cart-sauce-labs-backpack"]');
+  await page.click('[data-test ="add-to-cart-sauce-labs-bike-light"]');
+  
+
+
+  const cartBadge = await page.locator('.shopping_cart_badge');
+  await expect(cartBadge).toHaveText('2');
+});
+
+test('Logging out of inventory', async({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+
+  await expect(page).toHaveURL(/inventory/)
+
+  await page.click('#react-burger-menu-btn');
+  await page.click('[data-test="logout-sidebar-link"]')
+
+  await expect(page).toHaveURL('https://www.saucedemo.com/');
+  await expect(page.locator('#login-button')).toBeVisible();
+
+});
--- 03-automation-playwright/tests/login.spec.js
+++ 03-automation-playwright/tests/login.spec.js
@@ -9,3 +9,48 @@ test('Valid login test', async ({ page }) => {
 
   await expect(page).toHaveURL(/inventory/);
 });
+
+test('Invalid login test', async({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', 'invalid_password');
+  await page.click('#login-button');
+
+  const errorMessage = page.locator('[data-test="error"]');
+
+  await expect(errorMessage).toBeVisible();
+  await expect(errorMessage).toHaveText(
+    'Epic sadface: Username and password do not match any user in this service'
+  );
+});
+
+test('Empty username field', async({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+
+  await page.fill('#user-name', '');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+
+  const errorMessage = page.locator('[data-test="error"]');
+
+  await expect(errorMessage).toBeVisible();
+  await expect(errorMessage).toHaveText(
+    'Epic sadface: Username is required'
+  );
+});
+
+test('Empty password field', async({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+
+  await page.fill('#user-name', 'standard_user');
+  await page.fill('#password', '');
+  await page.click('#login-button');
+
+  const errorMessage = page.locator('[data-test="error"]');
+
+  await expect(errorMessage).toBeVisible();
+  await expect(errorMessage).toHaveText(
+    'Epic sadface: Password is required'
+  );
+});
